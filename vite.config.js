194167599e6c37e17199044659d8509239fbf693
Venugopal Reddy <nvenugopal@Venugopals-MacBook-Pro.local>
2026-05-24 12:36:03 -0500

    updated github pages deployment fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/codewithvenu/', // IMPORTANT: Replace with your actual repo name
+  base: '/', // IMPORTANT: Replace with your actual repo name
 })
